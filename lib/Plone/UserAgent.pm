ecf84e61fb4b1f132536b65e4c8959822712c29c
Dave Rolsky <autarch@urth.org>
2009-07-09 11:37:19 -0500

    Add docs

--- lib/Plone/UserAgent.pm
+++ lib/Plone/UserAgent.pm
@@ -133,23 +133,84 @@ __END__
 
 =head1 NAME
 
-Plone::UserAgent - The fantastic new Plone::UserAgent!
+Plone::UserAgent - An LWP agent with "logging into Plone" built-in
 
 =head1 SYNOPSIS
 
-XXX - change this!
-
     use Plone::UserAgent;
 
-    my $foo = Plone::UserAgent->new();
+    my $ua =
+        Plone::UserAgent->new
+            ( username => 'foo',
+              password => 'bar',
+              base_uri => 'http://my.plone.site.example.com',
+            );
 
-    ...
+    $ua->login();
+
+    my $page_uri = $ua->make_uri( '/some/page' );
+
+    my $response = $ua->get($page_uri);
 
 =head1 DESCRIPTION
 
+This module is a fairly trivial subclass of L<LWP::UserAgent> that
+knows how to log in to a Plone site. It's been tested with 3.2.2.
+
+Patches are welcome to add additional Plone-specific features (as long
+as they're nice patches, not gross, ugly patches).
+
 =head1 METHODS
 
-This class provides the following methods
+This class provides these methods;
+
+=head1 Plone::UserAgent->new( ... )
+
+This method creates a new user agent object.
+
+The constructor accepts the following parameters:
+
+=over 4
+
+=item * base_uri
+
+The root URI of your Plone site. B<required>.
+
+=item * username
+
+The username to use when logging in to the site. B<required>, but see
+below.
+
+=item * password
+
+The username to use when logging in to the site. B<required>, but see
+below.
+
+=item * config_file
+
+An optional config file. This should be an INI config file. All that
+is expected to be in the file is a username and password, with no
+section header:
+
+  username = foo
+  password = bar
+
+This defaults to F<$HOME/.plone-useragentrc>.
+
+=back
+
+The constructor requires a username and password, but if you don't
+pass these , it will try to look them up the config file.
+
+=head2 $ua->login()
+
+Attempts to log in to the site. Throws an error if it fails.
+
+=head2 $ua->make_uri($path)
+
+Given a path, returns a URI based on the C<base_uri> passed to the
+constructor. This is provided for convenience since it's used
+internally.
 
 =head1 AUTHOR
 
@@ -157,10 +218,31 @@ Dave Rolsky, E<gt>autarch@urth.orgE<lt>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-plone-useragent@rt.cpan.org>,
-or through the web interface at L<http://rt.cpan.org>.  I will be
-notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
+Please report any bugs or feature requests to
+C<bug-plone-useragent@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 DONATIONS
+
+If you'd like to thank me for the work I've done on this module,
+please consider making a "donation" to me via PayPal. I spend a lot of
+free time creating free software, and would appreciate any support
+you'd care to offer.
+
+Please note that B<I am not suggesting that you must do this> in order
+for me to continue working on this particular software. I will
+continue to do so, inasmuch as I have in the past, for as long as it
+interests me.
+
+Similarly, a donation made in this way will probably not make me work
+on this software much more, unless I get so many donations that I can
+consider working on free software full time, which seems unlikely at
+best.
+
+To donate, log into PayPal and send money to autarch@urth.org or use
+the button on this page:
+L<http://www.urth.org/~autarch/fs-donation.html>
 
 =head1 COPYRIGHT & LICENSE
 
